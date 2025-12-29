5878c3d084ecad58fde91024266fa48ddd262963
holynwk <164866527+holynwk@users.noreply.github.com>
2025-12-29 12:28:52 +0100

    Delete simpletown/src/main directory

--- simpletown/src/main/java/com/simpletown/war/WarResultOption.java
+++
@@ -1,8 +0,0 @@
-package com.simpletown.war;
-
-public enum WarResultOption {
-    TAKE_TERRITORIES,
-    TAKE_TREASURY,
-    TAKE_TECHNOLOGIES,
-    REPARATIONS
-}
--- simpletown/src/main/java/com/simpletown/war/WarType.java
+++
@@ -1,5 +0,0 @@
-package com.simpletown.war;
-
-public enum WarType {
-    WAR
-}
